1756a8d807e61d70f1daf27cecd32e0d0a3be888
rinnn31 <damquangphongthdk@gmail.com>
2026-05-07 11:45:01 +0700

    feat: config android's push notification

--- android/app/src/main/java/com/motelmanager/MainApplication.kt
+++ android/app/src/main/java/com/motelmanager/MainApplication.kt
@@ -6,6 +6,10 @@ import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import android.content.Context
 
 class MainApplication : Application(), ReactApplication {
 
@@ -23,5 +27,24 @@ class MainApplication : Application(), ReactApplication {
   override fun onCreate() {
     super.onCreate()
     loadReactNative(this)
+  }
+
+  fun createNotificationChannel() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val channelId = "motel_manager_channel"
+        val name = "Thông báo quan trọng"
+        val descriptionText = "Dùng cho các thông báo ưu tiên cao"
+        val importance = NotificationManager.IMPORTANCE_HIGH
+
+        val channel = NotificationChannel(channelId, name, importance).apply {
+            description = descriptionText
+            enableVibration(true)
+            vibrationPattern = longArrayOf(100, 200, 300, 400, 500)
+        }
+
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
   }
 }
